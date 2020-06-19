05351865331b4b38ddf843516f9cf741b3f562de
Andres Medrano <amedra13@gmail.com>
2020-06-19 00:00:53 -0700

    Started Order Summary Component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+            .map(igKey => {
+                return <li> <span syle={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+            });
+
+
+    return (
+        <>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+        </>
+    )
+};
+
+export default orderSummary;
--- src/components/containers/BurgerBuilder/BurgerBuilder.js
+++ src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import Burger from '../../Burger/Burger';
 import BuildControls from '../../Burger/BuildControls/BuildControls';
+import Modal from '../../UI/Modal/Modal';
+import OrderSummary from '../../Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -74,6 +76,7 @@ class BurgerBuilder extends Component{
 
         return (
             <>
+                
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler} 
@@ -81,6 +84,9 @@ class BurgerBuilder extends Component{
                     disabled={disabledInfo}
                     purchasable= {this.state.purchasable}
                     price={this.state.totalPrice}/>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
             </>
 
         );
