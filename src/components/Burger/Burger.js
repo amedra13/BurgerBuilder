0235f87f71b8f3d528845a364d8a030141de986d
Andres Medrano <amedra13@gmail.com>
2020-06-18 18:17:15 -0700

    check if ingredents are inputed

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,9 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) =>{
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map( igKey => [...Array(props.ingredients[igKey])]
-    .map( (_, i) => <BurgerIngredient key={igKey + i} type = {igKey}/> ));
+    .map( (_, i) => <BurgerIngredient key={igKey + i} type = {igKey}/> ))
+    .reduce ( (arr,el) => {
+        return arr.concat(el);
+    }, []);
+
+    console.log(transformedIngredients);
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients</p>
+    }
 
     return(
         <div className={classes.Burger}>
