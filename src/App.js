f10db3c2e3556d14b3f6d5847d94174a52847a48
Andres Medrano <amedra13@gmail.com>
2020-07-06 04:07:03 -0700

    fixed active page tab

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React ,{Component} from 'react';
 import Layout from './components/hoc/Layout/Layout'
 import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './components/containers/Checkout/Checkout';
+import Orders from './components/containers/Orders/Orders';
 import { Route , Switch} from 'react-router-dom';
 
 class App extends Component{
@@ -14,6 +15,7 @@ class App extends Component{
         <Layout>
           <Switch>
             <Route path='/checkout' component={Checkout} />
+            <Route path='/orders' component={Orders} />
             <Route path='/' exact component={BurgerBuilder}/>
           </Switch>
         </Layout>
