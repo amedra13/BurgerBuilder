becb1ecab6277e722256b7841e2ec1a727758189
Andres Medrano <amedra13@gmail.com>
2020-07-19 19:38:30 -0700

    file restructoring

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import rootReducer from './store/reducer';
+import rootReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const app =(
 
