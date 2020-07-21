211f7b508a201ae9a4d70cca86d6bdbc89a6f1d6
Andres Medrano <amedra13@gmail.com>
2020-07-20 17:49:52 -0700

    implement purchase burger state action creator

--- src/components/containers/Checkout/ContactData/ContactData.js
+++ src/components/containers/Checkout/ContactData/ContactData.js
@@ -99,7 +99,6 @@ class ContactData extends Component {
             }
         },
         formIsValid: false,
-        loading: false
 
     }
 
@@ -189,7 +188,7 @@ class ContactData extends Component {
             </form>
         )
 
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner />;
         }
         
@@ -206,13 +205,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return{
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
