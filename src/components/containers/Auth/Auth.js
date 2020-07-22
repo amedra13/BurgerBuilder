6d6f541f103e9e8994afbcdb0cdca5141b7f7876
Andres Medrano <amedra13@gmail.com>
2020-07-22 02:26:24 -0700

    create post method for firebase auth token

--- src/components/containers/Auth/Auth.js
+++ src/components/containers/Auth/Auth.js
@@ -104,7 +104,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form onSumbit={this.submitHandler}>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType='Success'>Submit</Button>
                 </form>
--- src/store/actions/Auth.js
+++ src/store/actions/Auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actions';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -22,5 +23,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart())
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAr_0UmkGq1-_QZMD6cM6gsJ71PmCXrsg0', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err))
+            })
     }
 }
