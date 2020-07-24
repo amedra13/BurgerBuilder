80f2d12513cf04091fb459d9a892c8296e5b6956
Andres Medrano <amedra13@gmail.com>
2020-07-24 00:41:55 -0700

    implement Guards and re-route to correct page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Checkout from './components/containers/Checkout/Checkout';
 import Orders from './components/containers/Orders/Orders';
 import Auth from './components/containers/Auth/Auth';
 import Logout from './components/containers/Auth/Logout/logout';
-import { Route , Switch, withRouter} from 'react-router-dom';
+import { Route , Switch, withRouter, Redirect} from 'react-router-dom';
 import * as actions from './store/actions/index';
 
 class App extends Component{
@@ -15,18 +15,33 @@ class App extends Component{
     this.props.onTryAutoSignUp()
   }
 
+
   render() {
+
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' exact component={BurgerBuilder}/>
+        <Redirect to='/' />
+      </Switch>
+    )
+
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/' exact component={BurgerBuilder}/>
+          <Redirect to='/' />
+        </Switch>
+      )
+    }
 
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path='/auth' component={Auth} />
-            <Route path='/logout' component={Logout} />
-            <Route path='/' exact component={BurgerBuilder}/>
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
@@ -35,10 +50,16 @@ class App extends Component{
 
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignUp: () => dispatch(actions.authCheckState())
   }
 }
 
-export default withRouter(connect(null,mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps,mapDispatchToProps)(App));
