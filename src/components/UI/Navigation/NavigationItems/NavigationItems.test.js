c8c48c4108d999bdf9039c1512cc6622b9b3483f
Andres Medrano <amedra13@gmail.com>
2020-07-25 23:42:47 -0700

    creating multiple tests and try different methods

--- src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,9 +7,30 @@ import NavItem from './NavItem/NavItem';
 
 configure({adapter: new Adapter()})
 
+let wrapper;
+
+beforeEach( () => {
+    wrapper = shallow(<NavigationItems />)
+})
+
 describe('<NavigationItems />', () => {
     it( ' should render two <NavItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems/>);
         expect(wrapper.find(NavItem)).toHaveLength(2)
+    })
+})
+
+describe('<NavigationItems />', () => {
+    it( ' should render three <NavItem /> elements if authenticated', () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated />)
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.find(NavItem)).toHaveLength(3)
+    })
+})
+
+
+describe('<NavigationItems />', () => {
+    it( 'should render logout tab', () => {
+        wrapper.setProps({isAuthenticated: true})
+        expect(wrapper.contains(<NavItem link ='/logout'>Logout</NavItem>)).toEqual(true)
     })
 })
