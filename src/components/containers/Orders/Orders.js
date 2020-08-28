3778e5217782e8d517a4f62d74a429adb3991003
Andres Medrano <amedra13@gmail.com>
2020-08-28 00:07:44 -0700

    Revert "change checkout and orders file using hooks"
    This reverts commit d9cd05b0920037ce2eba6603beb20cf794f05523.

--- src/components/containers/Orders/Orders.js
+++ src/components/containers/Orders/Orders.js
@@ -1,45 +1,53 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import {connect} from 'react-redux';
 import Order from '../../Order/Order';
 import axios from '../../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
 import Spinner from '../../UI/Spinner/Spinner';
 
-const Orders = (props) => {
-	useEffect(() => {
-		props.onFetchedOrders(props.token, props.userId);
-	}, [props]);
+class Orders extends Component {
 
-	let orders = <Spinner />;
-	if (!props.loading) {
-		orders = props.orders.map((order) => (
-			<Order
-				key={order.id}
-				ingredients={order.ingredients}
-				price={+order.price}
-			/>
-		));
-	}
-	return <div>{orders}</div>;
-};
+    state = {
+        loading: true
+    }
 
-const mapStateToProps = (state) => {
-	return {
-		token: state.auth.token,
-		orders: state.order.orders,
-		loading: state.order.loading,
-		userId: state.auth.userId,
-	};
-};
-const mapDispatchToProps = (dispatch) => {
-	return {
-		onFetchedOrders: (token, userId) =>
-			dispatch(actions.fetchOrders(token, userId)),
-	};
-};
+    componentDidMount() {
+        this.props.onFetchedOrders(this.props.token, this.props.userId);
+    }
+    render() {
+        let orders = <Spinner />;
+        if(!this.props.loading){
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(withErrorHandler(Orders, axios));
+            orders = this.props.orders.map(order => (
+                <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.price}/>
+            ))
+
+        }
+        return (
+            <div>
+                {orders}
+            </div>
+
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        token: state.auth.token,
+        orders: state.order.orders,
+        loading: state.order.loading,
+        userId: state.auth.userId
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        onFetchedOrders: (token, userId) => dispatch(actions.fetchOrders(token, userId))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
